a95a5fe1f421408877f0a1dab1d6617ee3ae04c9
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2021-04-21 00:36:36 +0200

    initial commit

---
+++ src/data/timetable.ts
@@ -0,0 +1,74 @@
+export const courses: any = [
+    {
+        name: "DS2",
+        groups: [
+            {
+                lecture: [0,7,9],
+                labs: [[0,5,5], [0,6,6]]
+            },
+            {
+                lecture: [4,7,9],
+                labs: [[4,3,3], [4,4,4]]
+            },
+            {
+                lecture: [4,10,12],
+                labs: [[2,5,5], [2,6,6]]
+            },
+            {
+                lecture: [0,10,12],
+                labs: [[0,7,7], [0,8,8]]
+            }
+        ]
+    },
+    {
+        name: "Signals",
+        groups: [
+            {
+                lecture: [0,3,5],
+                tutorial: [[0,0,0], [0,1,1]]
+            }
+        ]
+    },
+    {
+        name: "Computer Architecture",
+        groups: [
+            {
+                lecture: [2,6,8],
+                tutorials: [[5,0,0], [5,1,1]]
+            },
+            {
+                lecture: [2,6,8],
+                tutorials: [[1,0,0], [1,1,1]]
+            },
+            {
+                lecture: [5,5,7],
+                tutorials: [[5,9,9], [5,10,10]]
+            },
+            {
+                lecture: [3,6,8],
+                tutorials: [[4,5,5], [4,6,6]]
+            }
+        ]
+    },
+    {
+        name: "Systems Programming",
+        groups: [
+            {
+                lecture: [3,3,5],
+                tutorials: [[3,0,0], [3,1,1]]
+            },
+            {
+                lecture: [5,3,5],
+                tutorials: [[5,9,9], [5,10,10]]
+            },
+            {
+                lecture: [4,5,7],
+                tutorials: [[1,0,0], [1,1,1]]
+            },
+            {
+                lecture: [4,1,3],
+                tutorials: [[5,11,11], [5,12,12]]
+            }
+        ]
+    },
+]
